3ddaa62fdd213c944844f405256ad037cf24f518
shriramgokhale.m <shriramgokhale.m@planview.com>
2023-09-11 17:41:17 +0530

    Update configuration.py

--- streamlitapp/configuration.py
+++ streamlitapp/configuration.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utils import helper
 
-op_lst = ["GET", "POST"]
+op_lst = ["GET", "POST","PUT"]
 auth_lst = ["Basic", "Bearer"]
 menu_lst = ["Config", "Execution", "Results"]
 
@@ -26,10 +26,23 @@ def add_config_details(config_ids_list, default_config_index, selected_menu):
     config_details = helper.get_config_details(config_id)
     left, right = st.columns(2)
 
-    host = left.text_input("Host URL:", value=config_details["hostname"], placeholder="Enter host url")
     is_local_host = right.checkbox("Are you running on local host", value=config_details["isLocalhost"])
+
+    if  is_local_host:
+
+        host = left.text_input("Host URL:", value="http://localhost", placeholder="Enter host url", disabled=True)
+
+    else:
+
+        host = left.text_input("Host URL:", value=config_details["hostname"], placeholder="Enter host url")
+        
+
+    #is_local_host = right.checkbox("Are you running on local host", value=config_details["isLocalhost"])
     api_endpoint = left.text_input("API End Point:", value=config_details['endpoint'],
                                    placeholder="Enter end point with query parameters")
+    left.caption("Example: /planview/getAttributes")
+    
+
     operation = right.selectbox("Select Operation:", op_lst,
                                 index=op_lst.index(config_details['method']), key="select_operation")
     if operation == "POST":
@@ -52,6 +65,7 @@ def add_config_details(config_ids_list, default_config_index, selected_menu):
     elif auth_type == "Bearer":
         token = right.text_input("Token:", placeholder="Enter token", value=config_details["token"])
 
+<<<<<<< Updated upstream
     new_config_id = left.text_input("Config Name:", placeholder="Enter unique Name to save config details",
                                     value=config_id)
 
@@ -60,6 +74,14 @@ def add_config_details(config_ids_list, default_config_index, selected_menu):
                 "In general terms, more virtual users means more simulated traffic"
     duration = left.text_input("Duration:", placeholder="Enter duration in minutes", value=config_details["duration"])
     vus = left.text_input("No of vus:", placeholder="Enter duration in minutes", value=config_details["vus"], help=help_text)
+=======
+    new_config_id = left.text_input("Config Name :", placeholder="Enter unique Name to save config details",
+                                    value=config_id)
+
+    left_column, right_column = st.columns(2)
+    duration = left.text_input("Duration in minutes :", placeholder="Enter duration in minutes", value=config_details["duration"])
+    vus = left.text_input("No of Concurrent Users:", placeholder="Enter duration in minutes", value=config_details["vus"])
+>>>>>>> Stashed changes
 
     def save_config():
         helper.save_config(new_config_id, host, api_endpoint, operation,  is_local_host,
