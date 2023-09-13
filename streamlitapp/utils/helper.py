a3a551632cc3b2c6f741882b777440e77f65d9af
srajala <srajala@planview.com>
2023-09-13 10:31:09 +0530

    refactored

--- streamlitapp/utils/helper.py
+++ streamlitapp/utils/helper.py
@@ -36,7 +36,6 @@ def get_execution_list(config_id):
 
 
 def get_result_data(config_id, execution_id):
-
     metric_data = {}
 
     for execution in execution_id:
@@ -46,10 +45,14 @@ def get_result_data(config_id, execution_id):
 
             result_json = json.load(result_file)
 
-            metric_data[execution] = {"95th percentile response time in ms": round(result_json["metrics"]["RequestEndpointResponseTime"]["values"]["p(95)"], 2)}
-            metric_data[execution]["Average response time in ms"] = round(result_json["metrics"]["RequestEndpointResponseTime"]["values"]["avg"], 2)
-            metric_data[execution]["Request timeout Rate in Percentage"] = result_json["metrics"]["RequestEndpointRequestTimeoutRate"]["values"]["rate"] * 100
-            metric_data[execution]["Pass Rate in Percentage"] = result_json["metrics"]["RequestEndpointPassRate"]["values"]["rate"] * 100
+            metric_data[execution] = {"95th percentile response time in ms": round(
+                result_json["metrics"]["RequestEndpointResponseTime"]["values"]["p(95)"], 2)}
+            metric_data[execution]["Average response time in ms"] = round(
+                result_json["metrics"]["RequestEndpointResponseTime"]["values"]["avg"], 2)
+            metric_data[execution]["Request timeout Rate in Percentage"] = \
+                result_json["metrics"]["RequestEndpointRequestTimeoutRate"]["values"]["rate"] * 100
+            metric_data[execution]["Pass Rate in Percentage"] = \
+                result_json["metrics"]["RequestEndpointPassRate"]["values"]["rate"] * 100
             metric_data[execution]["Duration in min"] = result_json['TestSummary']['testData']['duration']
             metric_data[execution]["Concurrent Users"] = result_json["TestSummary"]["testData"]["vus"]
 
@@ -58,36 +61,6 @@ def get_result_data(config_id, execution_id):
             metric_data[execution] = {}
 
     return metric_data
-
-
-def save_config(config_id, host, api_endpoint, operation, is_local_host, payload, payload_type, payload_as_string,
-                auth_type, dsn, user_name, password, token, duration, vus):
-    config_dct = {
-        config_id: {
-            "hostname": host,
-            "endpoint": api_endpoint,
-            "method": operation,
-            "isLocalhost": is_local_host,
-            "payload": payload,
-            "payloadType": payload_type,
-            "payloadAsString": payload_as_string,
-            "auth": auth_type,
-            "dsn": dsn,
-            "username": user_name,
-            "password": password,
-            "token": token,
-            "duration": duration,
-            "vus": vus
-
-        }
-    }
-    config_file = open('.\\data\\config.json')
-
-    config_json = json.load(config_file)
-    config_json.update(config_dct)
-
-    with open(".\\data\\config.json", "w") as jsonfile:
-        json.dump(config_json, jsonfile)
 
 
 def update_config(config_id, config_details):
@@ -103,6 +76,28 @@ def update_config(config_id, config_details):
         json.dump(config_json, jsonfile)
 
 
+def save_config(config_id, host, api_endpoint, operation, is_local_host, payload, payload_type, payload_as_string,
+                auth_type, dsn, user_name, password, token, duration, vus):
+    config_details = {
+        "hostname": host,
+        "endpoint": api_endpoint,
+        "method": operation,
+        "isLocalhost": is_local_host,
+        "payload": payload,
+        "payloadType": payload_type,
+        "payloadAsString": payload_as_string,
+        "auth": auth_type,
+        "dsn": dsn,
+        "username": user_name,
+        "password": password,
+        "token": token,
+        "duration": duration,
+        "vus": vus
+
+    }
+    update_config(config_id, config_details)
+
+
 def validate_json(json_data):
     try:
         json.loads(json_data)
@@ -110,8 +105,8 @@ def validate_json(json_data):
         return False
     return True
 
-def delete_result(config_id, execid):
 
+def delete_result(config_id, execid):
     flag = os.remove('./resultlogs/' + config_id + '/' + execid + '.json')
 
     return flag
