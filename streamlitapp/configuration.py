e5e68afab959cb96dec853a8446f38047e8ba390
srajala <srajala@planview.com>
2023-09-15 13:52:02 +0530

    Added delete button

--- streamlitapp/configuration.py
+++ streamlitapp/configuration.py
@@ -15,7 +15,7 @@ def add_config_details(config_ids_list, default_config_index, selected_menu):
     password = ""
     token = ""
 
-    config_id = st.selectbox("Select Config:", config_ids_list,
+    config_id = st.selectbox(":blue[Select Config Name]", config_ids_list,
                              index=default_config_index, key="config_ids_list")
 
     st.session_state.config_id_selected = config_id
@@ -25,51 +25,52 @@ def add_config_details(config_ids_list, default_config_index, selected_menu):
     config_details = helper.get_config_details(config_id)
     left, right = st.columns(2)
 
-    is_local_host = right.checkbox("Are you running on local host", value=config_details["isLocalhost"])
+    is_local_host = right.checkbox(":blue[Are you running on local host]", value=config_details["isLocalhost"])
 
     if is_local_host:
 
-        host = left.text_input("Host URL:", value="http://localhost", placeholder="Enter host url", disabled=True)
+        host = left.text_input(":blue[Host URL]", value="http://localhost", placeholder="Enter host url", disabled=True)
 
     else:
 
-        host = left.text_input("Host URL:", value=config_details["hostname"], placeholder="Enter host url")
+        host = left.text_input(":blue[Host URL]", value=config_details["hostname"], placeholder="Enter host url")
 
-    api_endpoint = left.text_input("API End Point:", value=config_details['endpoint'],
+    api_endpoint = left.text_input(":blue[API End Point]", value=config_details['endpoint'],
                                    placeholder="Enter end point with query parameters")
     left.caption("Example: /planview/getAttributes")
 
-    operation = right.selectbox("Select Operation:", op_lst,
+    operation = right.selectbox(":blue[Select Operation]", op_lst,
                                 index=op_lst.index(config_details['method']), key="select_operation")
     if operation == "POST":
-        payload = left.text_area("Request Payload", value=config_details["payload"])
+        # payload = left.text_area("Request Payload", value=config_details["payload"])
+        payload = left.text_area(":blue[Request Payload]", value=config_details["payload"], height=10)
+
         if payload:
             status = helper.validate_json(payload)
             if not status:
                 st.error("Please provide valid JSON")
-        payload_type = right.selectbox("Payload Type",
+        payload_type = right.selectbox(":blue[Payload Type]",
                                        ("application/json", "application/x-www-form-urlencoded",
                                         "text/xml;charset=utf-8"))
-        payload_as_sting = right.checkbox("Payload as Sting", value=config_details["payloadAsString"])
+        payload_as_sting = right.checkbox(":blue[Payload as Sting]", value=config_details["payloadAsString"])
 
-    auth_type = right.selectbox("Authorization:", auth_lst, index=auth_lst.index(config_details['auth']))
+    auth_type = right.selectbox(":blue[Authorization]", auth_lst, index=auth_lst.index(config_details['auth']))
 
     if auth_type == "Basic":
-        dsn = right.text_input("DSN:", placeholder="Enter DSN Name", value=config_details["dsn"])
-        user_name = right.text_input("User Name:", placeholder="Enter User Name", value=config_details["username"])
-        password = right.text_input("Password:", placeholder="Enter Password", value=config_details["password"])
+        dsn = right.text_input(":blue[DSN]", placeholder="Enter DSN Name", value=config_details["dsn"])
+        user_name = right.text_input(":blue[User Name]", placeholder="Enter User Name", value=config_details["username"])
+        password = right.text_input(":blue[Password]", placeholder="Enter Password", value=config_details["password"])
     elif auth_type == "Bearer":
-        token = right.text_input("Token:", placeholder="Enter token", value=config_details["token"])
+        token = right.text_input(":blue[Token]", placeholder="Enter token", value=config_details["token"])
 
-    new_config_id = left.text_input("Config Name:", placeholder="Enter unique Name to save config details",
+    new_config_id = left.text_input(":blue[Config Name]", placeholder="Enter unique Name to save config details",
                                     value=config_id)
 
-    left_column, right_column = st.columns(2)
     help_text = "Test runs multiple iterations in parallel with virtual users (VUs). " \
                 "In general terms, more virtual users means more simulated traffic"
-    duration = left.text_input("Duration in minutes :", placeholder="Enter duration in minutes",
+    duration = left.text_input(":blue[Duration in minutes]", placeholder="Enter duration in minutes",
                                value=config_details["duration"])
-    vus = left.text_input("No of Concurrent Users :", placeholder="Enter duration in minutes",
+    vus = left.text_input(":blue[No of Concurrent Users]", placeholder="Enter duration in minutes",
                           value=config_details["vus"], help=help_text)
 
     def save_config():
@@ -77,7 +78,42 @@ def add_config_details(config_ids_list, default_config_index, selected_menu):
                            payload, payload_type, payload_as_sting, auth_type, dsn, user_name, password, token,
                            duration, vus)
 
-    if left_column.button('Save Config:'):
+    button1_css = """
+    <style>
+    #custom-button-1>button {
+        background-color: #17e84f;
+        color: white;
+        border-color: #17e84f;
+    }
+    </style>
+    """
+
+    button2_css = """
+    <style>
+    #custom-button-2>button {
+        background-color: #e81717;
+        color: white;
+        border-color: #e81717;
+    }
+    </style>
+    """
+
+    button3_css = """
+    <style>
+     #custom-button-3>button {
+         background-color: #17e84f;
+         color: white;
+         border-color: #17e84f;
+     }
+     </style>
+     """
+    left_column, center_column, right_column = st.columns([0.1, 0.1, 0.5])
+
+    left_column.markdown(button1_css, unsafe_allow_html=True)
+    right_column.markdown(button2_css, unsafe_allow_html=True)
+    center_column.markdown(button3_css, unsafe_allow_html=True)
+
+    if left_column.button("Save", key="custom-button-1"):
         if new_config_id in config_ids_list:
             st.error(f"Config with {new_config_id} name already exists please provide new id")
         else:
@@ -87,11 +123,11 @@ def add_config_details(config_ids_list, default_config_index, selected_menu):
 
     st.session_state.disabled = True
 
-    if token != config_details['token'] or dsn != config_details['dsn'] or user_name != config_details['username'] or \
-            password != config_details['password'] or duration != config_details['duration'] or vus != config_details['vus']:
+    if token != config_details['token'] or dsn != config_details['dsn'] or \
+            user_name != config_details['username'] or password != config_details['password']:
         st.session_state.disabled = False
 
-    update_button = placeholder.button('Update Config:', disabled=st.session_state.get('disabled'), key=1)
+    update_button = placeholder.button('Update', disabled=st.session_state.get('disabled'), key='custom-button-2')
 
     if update_button:
         config_details['token'] = token
@@ -100,6 +136,9 @@ def add_config_details(config_ids_list, default_config_index, selected_menu):
         config_details['password'] = password
         config_details['duration'] = duration
         config_details['vus'] = vus
-        placeholder.button('Update Config:', disabled=True, key=2)
+        placeholder.button('Update', disabled=True, key="custom-button-2")
 
         helper.update_config(config_id, config_details)
+
+    if center_column.button("Delete", key="custom-button-3"):
+        pass
