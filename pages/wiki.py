5cc725d8d784c594729f38a9d1028d4d0bf6cbce
shriramgokhale.m <shriramgokhale.m@planview.com>
2023-11-23 16:23:22 +0530

    Added wiki page for how use scenarios

---
+++ pages/wiki.py
@@ -0,0 +1,12 @@
+import streamlit as st
+  
+st.set_page_config(page_title = "Wiki Page") 
+
+#Title
+st.title("How to use the tool")
+
+#section1
+st.header("Getting Started")
+st.write("")
+st.code("")
+#stm.sidebar.success("Select Any Page from here") 
